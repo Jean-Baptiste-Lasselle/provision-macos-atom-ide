d53b5c4ee08744ec7311bb3144e9cd6ea0ae9530
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-03-19 10:02:49 +0100

    Update operations.sh

--- operations.sh
+++ operations.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+export MAISON_OPS=~/.pegasus.ide
+
 export VERSION_ATOM_IDE=xxx
 
 
-sudo apt-get install -y atom
+mkdir -p $MAISON_OPS
+cd $MAISON_OPS
 
+
+# 1ère méthode de la doc officielle : ne fonctionne pas sur debain stretch
+# sudo apt-get install -y atom
+
+#2ième
+export URI_TELECHARGEMENT_PKG_DEBIAN=https://atom.io/download/deb
+
+
+
+
+wget $URI_TELECHARGEMENT_PKG_DEBIAN
+# le nom du packag ne varie jamais avecles versions ... ouh je sens que je vais les aimer, eux.
+sudo dpkg -i atom-amd64.deb
+sudo apt-get -f install
 # et c'est tout...
